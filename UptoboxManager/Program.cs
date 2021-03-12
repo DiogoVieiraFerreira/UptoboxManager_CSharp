a9566020369aed3c87c4c4b933e8ed4e25e989b3
Noxcaedibux <diogof-6@hotmail.com>
2021-03-12 17:41:28 +0100

    initial commit

---
+++ UptoboxManager/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UptoboxManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
